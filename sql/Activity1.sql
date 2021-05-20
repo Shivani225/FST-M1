4abba65d9876d73abd69bd674bc26e6a3fa00082
Shivani Hembrom <68163424+Shivani225@users.noreply.github.com>
2021-05-21 03:29:47 +0530

    Add files via upload

---
+++ sql/Activity1.sql
@@ -0,0 +1,12 @@
+-- Activity1
+create table salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(20), 
+    salesman_city varchar2(20), 
+    commission int 
+    );
+
+select * from salesman;
+desc salesman
+
+
---
+++ sql/Activity3.sql
@@ -0,0 +1,8 @@
+Select salesman_id,salesman_city from salesman;
+	
+Select * from salesman 
+where salesman_city = 'Paris';
+
+select salesman_id,commission from salesman 
+where salesman_name = 'Paul Adam';
+
